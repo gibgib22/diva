e3713552afebcd494bb99fa927e1295684ac5121
gibgib22 <78519249+gibgib22@users.noreply.github.com>
2021-02-11 14:29:13 +0700

    Add files via upload

---
+++ belajar_css/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BELAJAR CSS</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+	<body>
+		<header class="judul">
+			<center>
+				<h1>----CIPLUS----</h1>
+				<h>Sistem Informasi Perpustakaan </h2>
+			</center>
+		</header>
+		<header class="header">
+			<div class="menu">
+				<ul>
+					<li><a href="">home</a></li>
+					<li><a href="">Data siswa</a></li>
+					<li><a href="">Data buku</a></li>
+					<li><a href="">Data peminjaman</a></li>
+					<li><a href="">about</a></li>
+				</ul>
+			</div>
+		</header>
+		<br/>
+
+		<content class="content">
+			<center>
+				<h1>Selamat Datang di Perpustakaan Telkom</h1>
+				<h2>SMK Telkom Malang</h2>
+				<img src="logo.jpg" style="width: 128px; height: 128px;">
+			</center>
+		</content>
+ 
+		<center>
+		<table>
+			<tr>
+				<th>NO</th>
+				<th>Nama</th>
+				<th>NIS</th>
+				<th>Kelas</th>
+				<th>Jurusan</th>
+				<th>Email</th>
+				<th>Aksi</th>
+			</tr>
+			<tr>
+				<td>1.</td>
+				<td>Divana Anggita Putri</td>
+				<td>10101001001</td>
+				<td>X RPL 7</td>
+				<td>RPL</td>
+				<td>Divana.putri13@gmail.com</td>
+				<td>
+					<a href="">edit</a> ||
+					<a href="">hapus</a>
+				</td>
+			</tr>
+			<tr>
+				<td>2.</td>
+				<td>Divana Anggita Putri</td>
+				<td>10101001001</td>
+				<td>X RPL 7</td>
+				<td>RPL</td>
+				<td>Divana.putri13@gmail.com</td>
+				<td>
+					<a href="">edit</a> ||
+					<a href="">hapus</a>
+				</td>
+			</tr>
+			<tr>
+				<td>3.</td>
+				<td>Divana Anggita Putri</td>
+				<td>10101001001</td>
+				<td>X RPL 7</td>
+				<td>RPL</td>
+				<td>Divana.putri13@gmail.com</td>
+				<td>
+					<a href="">edit</a> ||
+					<a href="">hapus</a>
+				</td>
+			</tr> 
+		</table>
+	</center> -->
+<!-- 
+		<h1>Bimillah</h1> -->
+
+		<p>Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah Bimillah </p>
+		<br/>
+		<footer class="footer">
+			<center>
+				<h3>CopyRight @Divana</h3>
+			</center>
+		</footer>
+	</body>
+
+
+</html>
